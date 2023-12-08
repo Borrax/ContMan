2d97a1d0f9e4a497bd2c7edf8a29a048e70d3fa1
Borrax <borrax@protonmail.com>
2023-12-08 13:22:40 +0200

    Initializing the app window

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from tkinter import Tk
+
+root = Tk()
+root.title('Content Manager')
+root.geometry('400x400')
+root.resizable(0, 0)
+root.columnconfigure(0, weight=1)
+
+root.mainloop()
