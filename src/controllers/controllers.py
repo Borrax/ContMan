cd53e91cdaefb2061edac9df3f4e153de53a386b
Borrax <borrax@protonmail.com>
2023-12-08 23:47:27 +0200

    Removing the abstract method decorators in the BaseController

--- src/controllers/controllers.py
+++ src/controllers/controllers.py
@@ -20,23 +20,22 @@ class BaseController(ABC):
     db_provider = None
     target_collection = None
 
+    @abstractmethod
+    def __init__(self, db_provider: DbProvider, target_collection: str):
+        self.db_provider = db_provider
+        self.target_collection = target_collection
+
     def tryexceptwrap(func):
         try:
             return func
         except Exception as e:
             return FailedReq(repr(e))
 
-    def __init__(self, db_provider: DbProvider, target_collection: str):
-        self.db_provider = db_provider
-        self.target_collection = target_collection
-
-    @abstractmethod
     @tryexceptwrap
     def get_collection(self) -> ControllerMethodOutput:
         collection = self.db_provider.get_db_json()[self.target_collection]
         return FulfilledReq(collection)
 
-    @abstractmethod
     def add_item(self, item) -> ControllerMethodOutput:
         try:
             db_json = self.db_provider.get_db_json()
@@ -47,11 +46,9 @@ class BaseController(ABC):
         except Exception as e:
             return FailedReq(repr(e))
 
-    @abstractmethod
     def delete_item(self, id) -> ControllerMethodOutput:
         pass
 
-    @abstractmethod
     def edit_item(self, id, new_entry) -> ControllerMethodOutput:
         pass
 
@@ -59,15 +56,3 @@ class BaseController(ABC):
 class GamesController(BaseController):
     def __init__(self, db_provider):
         super().__init__(db_provider, 'games')
-
-    def add_item(self, item):
-        return super().add_item(item)
-
-    def edit_item(self, id: str):
-        return super().add_item(id)
-
-    def delete_item(self, id: str):
-        return super().delete_item(id)
-
-    def get_collection(self):
-        return super().get_collection()
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import Tk
-# import controllers.controllers
+import controllers.controllers
 
 root = Tk()
 root.title('Content Manager')
