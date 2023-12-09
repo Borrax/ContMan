93dbe80205187a84ecad6d549eb4a8fd9ed985e1
Borrax <borrax@protonmail.com>
2023-12-09 02:01:30 +0200

    Redefining the try/except wrapper in the base controller

--- src/controllers/controllers.py
+++ src/controllers/controllers.py
@@ -3,6 +3,7 @@ from typing import Union
 
 from db_provider import DbProvider
 from models.models import BaseItem
+from utils.decorators import tryexceptwrap_builder
 
 type ControllerMethodOutput = Union[FulfilledReq, FailedReq]
 
@@ -18,49 +19,49 @@ class FailedReq:
 
 
 class BaseController(ABC):
-    db_provider = None
-    target_collection = None
+    __db_provider = None
+    __target_collection = None
+    __tryexceptwrap = tryexceptwrap_builder(
+        lambda e: FailedReq(e)
+    )
 
     @abstractmethod
     def __init__(self, db_provider: DbProvider, target_collection: str):
         self.db_provider = db_provider
         self.target_collection = target_collection
 
-    def tryexceptwrap(func):
-        try:
-            return func
-        except Exception as e:
-            return FailedReq(repr(e))
-
-    @tryexceptwrap
+    @__tryexceptwrap
     def get_collection(self) -> ControllerMethodOutput:
-        collection = self.db_provider.get_db_json()[self.target_collection]
+        collection = self.__db_provider.get_db_json()[self.__target_collection]
         return FulfilledReq(collection)
 
-    @tryexceptwrap
+    @__tryexceptwrap
     def add_item(self, item) -> ControllerMethodOutput:
-        db_json = self.db_provider.get_db_json()
-        db_json[self.target_collection].add(item)
-        self.db_provider.rewrite_db(db_json)
+        db_json = self.__db_provider.get_db_json()
+        db_json[self.__target_collection].add(item)
+        self.__db_provider.rewrite_db(db_json)
         return FulfilledReq()
 
-    @tryexceptwrap
+    @__tryexceptwrap
     def delete_item(self, id) -> ControllerMethodOutput:
-        db_json = self.db_provider.get_db_json()
-        del db_json[self.target_collection][id]
+        db_json = self.__db_provider.get_db_json()
+        del db_json[self.__target_collection][id]
         return FulfilledReq()
 
-    @tryexceptwrap
+    @__tryexceptwrap
     def edit_item(self, new_item: BaseItem) -> ControllerMethodOutput:
         id = new_item.id
         db_json = self.db_provider.get_db_json()
-        if db_json[self.target_collection].get(id) is None:
+        if db_json[self.__target_collection].get(id) is None:
             return FailedReq('Not existing id of the item')
 
-        db_json[self.target_collection][id] = new_item
+        db_json[self.__target_collection][id] = new_item
         return FulfilledReq()
 
 
 class GamesController(BaseController):
     def __init__(self, db_provider):
         super().__init__(db_provider, 'games')
+
+
+GamesController(DbProvider)
